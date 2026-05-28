880c9941ed569966bc692bceb528e321e89ceaec
jaaccc <djacobmohamed@gmail.com>
2026-05-28 18:22:18 -0400

    completed raw mode implementation

--- jtxt.c
+++ jtxt.c
@@ -1,27 +1,56 @@
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
 struct termios original;
 
-void disableRaw(void) { tcsetattr(STDIN_FILENO, TCSAFLUSH, &original); }
+void kill(const char *s) {
+    perror(s);
+    exit(1);
+}
+
+void disableRaw(void) {
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &original) == -1)
+        kill("scsetattr");
+}
 
 void enableRaw(void) {
-    tcgetattr(STDIN_FILENO, &original);
+    if (tcgetattr(STDIN_FILENO, &original) == -1)
+        kill("tcgetattr");
     atexit(disableRaw);
 
     struct termios raw = original;
 
-    raw.c_lflag &= ~(ECHO | ICANON);
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    raw.c_cflag |= (CS8);
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    raw.c_oflag &= ~(OPOST);
+
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
+
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
+        kill("tcsetattr");
 }
 
 int main(void) {
     enableRaw();
 
-    char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
-        ;
+    while (1) {
+        char c = '\0';
+
+        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
+            kill("read");
+        if (iscntrl(c))
+            printf("%d\r\n", c);
+        else
+            printf("%d ('%c')\r\n", c, c);
+        if (c == 'q')
+            break;
+    }
 
     return 0;
 }
